9b5856a79d4945d41c6f979cecd95229e7afd489
Nafis Ahamed <nafisahamed14@gmail.com>
2024-04-03 21:07:43 +0600

    first commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHfP3y3xZHx1dq2uChAiquSCmJKMGRpsM",
+  authDomain: "module-52-a0f9f.firebaseapp.com",
+  projectId: "module-52-a0f9f",
+  storageBucket: "module-52-a0f9f.appspot.com",
+  messagingSenderId: "1042639983862",
+  appId: "1:1042639983862:web:de20f9d287d275eb3c88d7"
+};
+
+const app = initializeApp(firebaseConfig);
