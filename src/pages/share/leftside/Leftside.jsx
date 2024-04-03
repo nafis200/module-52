e8f10e5dbea049de7fa5139d22816b382a707d80
Nafis Ahamed <nafisahamed14@gmail.com>
2024-04-04 01:59:45 +0600

    half done

--- src/pages/share/leftside/Leftside.jsx
+++ src/pages/share/leftside/Leftside.jsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 
 
 const Leftside = () => {
+   
+    const [categories,setCategories] = useState([])
+
+    useEffect(()=>{
+         fetch('categories.json')
+         .then(res => res.json())
+         .then(data => setCategories(data))
+    },[])
+
     return (
-        <div>
-            <h2 className="text-2xl">Left side</h2>
+        <div className="space-y-6">
+            <h2 className="text-2xl">Left side:All category</h2>
+            {
+                 categories.map(category => <Link className="block ml-4 text-xl font-semibold " key={category.id}
+                 to ={`/category/${category.id}`}
+                 >{category.name}</Link> )
+            }
         </div>
     );
 };
